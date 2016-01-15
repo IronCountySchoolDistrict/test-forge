ce4a4f7e1663fba71897209daaf3880798bd9139
Nathan Jones <Purkinje90@gmail.com>
2016-01-15 15:32:42 -0700

    -Reformatting -Refactored promptHandlerFile so it can more cleanly handle two different test types from a file source

--- src/prompt.js
+++ src/prompt.js
@@ -1,18 +1,14 @@
 require('babel-polyfill');
 
 import { prompt } from 'inquirer';
-import {
-  getMatchingTests,
-  getCrtTestResults,
-  getCrtProficiency
-}
-from './service';
+import { getMatchingTests, getCrtTestResults, getCrtProficiency } from './service';
 import detect from './detector';
 import transform from './transform';
 import json2csv from 'json2csv';
 // import { workflow } from './workflow';
 import * as crt from './crt';
 import * as dibels from './dibels';
+import * as sage from './sage';
 import { Observable } from '@reactivex/rxjs';
 import Bluebird from 'bluebird';
 
@@ -20,55 +16,74 @@ var toCSV = Bluebird.promisify(json2csv);
 
 function asyncPrompt(questions) {
   return new Promise((resolve, reject) => {
-    prompt(questions, function(answers) {
+    prompt(questions, function (answers) {
       resolve(answers);
     });
   });
+}
+
+async function promptTestId(test) {
+
+  let matchingTests = await getMatchingTests(test.name);
+  let testChoices = matchingTests.rows.map(test => ({
+    name: test.NAME,
+    value: test.ID
+  }));
+  let testQuestion = {
+    type: 'list',
+    name: 'id',
+    message: 'Which test would you like to use for the import?',
+    choices: testChoices
+  };
+
+  let importTest = await asyncPrompt(testQuestion);
+  console.log(`Using test with id: ${importTest.id}`);
+  return importTest.id;
+}
+
+async function promptTable() {
+  // TODO: Make this list dynamic, based on the test type of the data source.
+  let importQuestion = {
+    type: 'list',
+    name: 'table',
+    message: 'Which table/data set are you forging import data for?',
+    choices: [
+      {
+        name: 'Test Results',
+        value: 'Test Results'
+      },
+      {
+        name: 'U_StudentTestProficiency',
+        value: 'U_StudentTestProficiency'
+      }
+    ]
+  };
+
+  let importTable = await asyncPrompt(importQuestion);
+  console.log(`Creating test data for table/data set: ${importTable.table}`);
+  return importTable.table;
 }
 
 export async function promptHandlerFile(source, file) {
   try {
     source.take(1).subscribe(async function(csvData) {
       let test = detect(csvData);
-      let matchingTests = await getMatchingTests(test.name);
-      let testChoices = matchingTests.rows.map(test => ({
-        name: test.NAME,
-        value: test.ID
-      }));
 
-      let testQuestion = {
-        type: 'list',
-        name: 'tests',
-        message: 'Which test would you like to use for the import?',
-        choices: testChoices
-      };
-
-      let tests = await asyncPrompt(testQuestion);
-      console.log(`Using test with id: ${tests.tests}`);
-      let testId = tests.tests;
-
-      // TODO: Make this list dynamic, based on the type of data source given.
-      let importQuestion = {
-        type: 'list',
-        name: 'table',
-        message: 'Which table/set are you forging import data for?',
-        choices: [{
-          name: 'Test Results',
-          value: 'Test Results'
-        }, {
-          name: 'U_StudentTestProficiency',
-          value: 'U_StudentTestProficiency'
-        }]
-      };
-
-      let table = await asyncPrompt(importQuestion);
-      console.log(`Creating test data for table: ${table.table}`);
-      let promptResps = {
-        testId: testId,
-        table: table.table
-      };
-
-      let workflow = dibels.createWorkflow(source, promptResps, file);
+      if (test.name === 'ROGL') {
+        let testId = await promptTestId(test);
+        let table = await promptTable();
+        let promptResps = {
+          testId: testId,
+          table: table
+        };
+        var workflow = dibels.createWorkflow(source, promptResps, file);
+      } else if (test.name === 'SAGE') {
+        let table = await promptTable();
+        let promptResps = {
+          table: table
+        }
+        var workflow = sage.createWorkflow(source, promptResps);
+      }
       workflow.start();
     });
   } catch (e) {
@@ -78,15 +93,15 @@ export async function promptHandlerFile(source, file) {
 
 export async function promptHandlerSams(test) {
   try {
-      // TODO: Make this list dynamic, based on the type of data source given.
-      let importQuestion = {
-        type: 'list',
-        name: 'table',
-        message: 'Which table/set are you forging import data for?',
-        choices: [{
-          name: 'Test Results',
-          value: 'Test Results'
-        }, {
+    // TODO: Make this list dynamic, based on the type of data source given.
+    let importQuestion = {
+      type: 'list',
+      name: 'table',
+      message: 'Which table/set are you forging import data for?',
+      choices: [{
+        name: 'Test Results',
+        value: 'Test Results'
+      }, {
           name: 'U_StudentTestProficiency',
           value: 'U_StudentTestProficiency'
         }, {
@@ -96,23 +111,23 @@ export async function promptHandlerSams(test) {
           name: 'U_TestSubscore',
           value: 'U_TestSubscore'
         }]
-      };
+    };
 
-      let table = await asyncPrompt(importQuestion);
-      console.log(`Creating test data for table: ${table.table}`);
-      let promptResps = {
-        table: table.table
-      };
+    let table = await asyncPrompt(importQuestion);
+    console.log(`Creating test data for table: ${table.table}`);
+    let promptResps = {
+      table: table.table
+    };
 
-      let source;
-      if (promptResps.table === 'Test Results') {
-        source = await getCrtTestResults();
-      } else {
-        source = await getCrtProficiency();
-      }
+    let source;
+    if (promptResps.table === 'Test Results') {
+      source = await getCrtTestResults();
+    } else {
+      source = await getCrtProficiency();
+    }
 
-      let workflow = crt.createWorkflow(source, promptResps);
-      workflow.start();
+    let workflow = crt.createWorkflow(source, promptResps);
+    workflow.start();
   } catch (e) {
     console.error(e.stack);
   }
