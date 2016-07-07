01a169f0f16829c27b16099d97998fc2a7dfed69
Nathan Jones <nathan.jones@ironmail.org>
2016-07-07 15:30:53 -0600

    don't log that we're logging errors

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,6 @@ import { logger } from './index';
 import { printObj } from './util';
 
 export function logErrors(item, msg, e) {
-  console.log('in logErrors');
   logger.log('info', msg, {
     psDbError: printObj(e)
   });
